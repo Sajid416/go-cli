e9fe660174b70ee510002ad8dbf65d2a9901d641
Sajid416 <sajidulislam6183@gmail.com>
2025-10-14 22:37:30 +0600

    feature3 comment has been added

--- pkg/parser/benchmark_test.go
+++ pkg/parser/benchmark_test.go
@@ -5,7 +5,7 @@ import "testing"
 func BenchmarkParseCSV(b *testing.B) {
 	data := "a,b,c\n1,2,3\n4,5,6\n7,8,9\n"
 	p := CSVParser{}
-	b.ResetTimer()
+	b.ResetTimer() //feature
 	for i := 0; i < b.N; i++ {
 		_, _ = p.Parse([]byte(data))
 	}
